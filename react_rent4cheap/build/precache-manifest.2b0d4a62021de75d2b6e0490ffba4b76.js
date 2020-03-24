31c8f5cbd74a7339ec98d960d7af979db6220ed4
gurshant <gurshant53@gmail.com>
2020-03-23 19:54:31 -0700

    added more lenient middleware to attempt a fix in cors bug

---
+++ react_rent4cheap/build/precache-manifest.2b0d4a62021de75d2b6e0490ffba4b76.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ee07f22f984db4fd9c349372303f84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "603930807b527de60474",
+    "url": "/static/css/main.79619a3a.chunk.css"
+  },
+  {
+    "revision": "1e49498f32d0632c0149",
+    "url": "/static/js/2.0cdda878.chunk.js"
+  },
+  {
+    "revision": "0072bd2b73ba58a22325ab4c04c8502a",
+    "url": "/static/js/2.0cdda878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603930807b527de60474",
+    "url": "/static/js/main.16fe2519.chunk.js"
+  },
+  {
+    "revision": "2ba8900929231c2fae0e",
+    "url": "/static/js/runtime-main.c9092cb2.js"
+  },
+  {
+    "revision": "55f513595ec6280b864256b7b2586874",
+    "url": "/static/media/building.55f51359.jpg"
+  },
+  {
+    "revision": "52c8e49937677b1de97b3a55aa97f690",
+    "url": "/static/media/family.52c8e499.jpg"
+  },
+  {
+    "revision": "68c9605097a23691bcfdbd9aca012d12",
+    "url": "/static/media/home1.68c96050.jpg"
+  },
+  {
+    "revision": "8102d44813b11408e4128f8569832e33",
+    "url": "/static/media/home2.8102d448.jpg"
+  },
+  {
+    "revision": "23aa86ab61e4afc3d84cb6df4b2a0536",
+    "url": "/static/media/home3.23aa86ab.jpg"
+  },
+  {
+    "revision": "968446f6edd1aff6493de58d59065239",
+    "url": "/static/media/home4.968446f6.jpg"
+  },
+  {
+    "revision": "3f58b910eb11d7b97aa024762a4d89db",
+    "url": "/static/media/logo.3f58b910.png"
+  },
+  {
+    "revision": "481d21961d62e6f58f2dc8260e9f8c51",
+    "url": "/static/media/marker.481d2196.svg"
+  }
+]);
